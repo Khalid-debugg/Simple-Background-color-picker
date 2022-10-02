9fe69fda8bc7eac6e2bc0a702d522f1edc1ae848
Khalid-debugg <62448116+Khalid-debugg@users.noreply.github.com>
2022-10-02 20:25:27 +0200

    Update app2.js

--- 1-color changer/shuffle/app2.js
+++ 1-color changer/shuffle/app2.js
@@ -1,19 +1,40 @@
 const hex=['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+const colors=['red','green','blue','white','cyan','grey']
 const btn= document.querySelector('button')
 const body= document.getElementsByTagName('main')
 const p= document.createElement('p')
-var hexColor='#'
+
 
 btn.addEventListener('click', ()=>{
-    for(let i=0;i<6;i++){
-        hexColor+=hex[getRand()]
+    var check=Math.floor(Math.random()*3)
+
+    if(check==0){
+        colorOutput='#'
+        for(let i=0;i<6;i++){
+            colorOutput+=hex[getRand(hex.length)]
+        }
     }
-    document.body.style.backgroundColor=hexColor
-    p.textContent=`Current backgound color is ${hexColor}`
+    else if(check==1){
+        colorOutput='rgb('
+        for(let i=0;i<3;i++){
+            if(i==2){
+                colorOutput+=getRand(226)+`)`;
+            }
+            else{
+                colorOutput+=getRand(226)+`, `;
+            }
+        }
+    }
+    else{
+        let rand=getRand(colors.length)
+        colorOutput=colors[rand]
+    }
+    document.body.style.backgroundColor=colorOutput
+    p.textContent=`Current backgound color is ${colorOutput}`
     body[0].appendChild(p)
-    hexColor='#'
+    
 })
 
-function getRand(){
-    return Math.floor(Math.random()*hex.length)
+function getRand(length){
+    return Math.floor(Math.random()*length)
 }
